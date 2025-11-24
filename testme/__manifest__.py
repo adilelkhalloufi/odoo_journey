5cbc43f11978ad370c1f0543f67e8bc76db4b44a
adil elkhalloufi <adilelkhalloufi@gmail.com>
2025-11-24 12:13:23 +0100

    first commit

---
+++ testme/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "TestMe",
+    "version": "1.0.0",
+    "summary": "Module de test pour démonstration",
+    "description": """TestMe — module d'exemple contenant des modèles, vues et données de démonstration.""",
+    "author": "Adev",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+
+    ],
+     "application": true,
+
+
+}
